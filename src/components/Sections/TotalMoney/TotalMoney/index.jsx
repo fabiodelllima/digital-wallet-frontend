7ac1ac4da73f1ea88c57c321dbc422759388369e
fabiodelllima <f.f.fabiodelima@gmail.com>
2023-08-01 17:12:11 -0300

    feat: Cálculo do total de acordo com os tipos de valores

--- src/components/Sections/TotalMoney/TotalMoney/index.jsx
+++ src/components/Sections/TotalMoney/TotalMoney/index.jsx
@@ -1,20 +1,25 @@
 import styles from './style.module.scss';
 
-export const TotalMoney = ({ cardList }) => {  
+export const TotalMoney = ({ cardList }) => {
   const calc = () => {
     if (cardList.length === Number(0)) {
       return Number(0);
     }
-    
-    const total = cardList.reduce((acc, card) => acc + Number(card.value), Number(0));
+
+    const total = cardList.reduce((acc, card) => {
+      return card.type === 'income' 
+      ? acc + Number(card.value) 
+      : acc - Number(card.value);
+    }, Number(0));
+
     return total;
   };
-  
+
   const formattedTotal = calc().toLocaleString(
     'pt-BR', { style: 'currency', currency: 'BRL' }
   );
 
-  return(
+  return (
     <section className={styles.container}>
       <div className={styles.header}>
         <p className='title-3'>Valor total:</p>
